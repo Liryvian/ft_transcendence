da9d163abf29e05368795a403d38ee2a004a3c0d
Vincent <Codam>
2022-12-05 14:20:40 +0100

    make sure pinia is added during npm create

--- setup_scripts/create_clean_frontend.sh
+++ setup_scripts/create_clean_frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm create vue@3 -y -- --project-name "frontend" --typescript --vue-router --vitest --playwright --eslint-with-prettier
+npm create vue@3 -y -- --project-name "frontend" --typescript --vue-router --vitest --playwright --eslint-with-prettier --pinia
 cd frontend
 npm install
