12a6c0c1362c965c14a4d050c3f9689f370b34f4
Vincent <Codam>
2022-12-07 11:41:58 +0100

    - reset nginx to default config version - remove set upstream variable from nginx (not needed, did not solve anything) - add host+port to vite setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    strictPort: true,
+    port: 5173
   },
   resolve: {
     alias: {
