3ffdf8594ee22b52ea3ea525435fb6a180ce23d5
Ryno95 <rmeiboom@student.codam.nl>
2022-12-08 17:55:35 +0100

    removed env hardcoded env vars and unneeded imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestExampleModule } from './test_example/test_example.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
   imports: [
-    TestExampleModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'database',
-      port: 5432,
-      username: 'admin_m2fVLBU',
-      password: 'zj3Bbgy6njq2FfQc',
-      database: 'transcendence',
-      autoLoadEntities: true,
-      synchronize: true,
   }),
 ],
   controllers: [AppController],
