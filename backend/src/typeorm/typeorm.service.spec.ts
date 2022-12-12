218edad886a42952831180714768e30eef2ffa12
Vincent <Codam>
2022-12-12 17:29:40 +0100

    convert to tabs

--- backend/src/typeorm/typeorm.service.spec.ts
+++ backend/src/typeorm/typeorm.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeormService } from './typeorm.service';
 
 describe('TypeormService', () => {
-  let service: TypeormService;
+	let service: TypeormService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TypeormService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [TypeormService],
+		}).compile();
 
-    service = module.get<TypeormService>(TypeormService);
-  });
+		service = module.get<TypeormService>(TypeormService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
