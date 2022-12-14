7fff6284655a9afbee210d9e7c6cbeb9bea4d1de
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-12-14 18:12:33 +0100

    a

--- frontend/src/pages/seasnails/Seasnails.vue
+++ frontend/src/pages/seasnails/Seasnails.vue
@@ -1,10 +1,24 @@
 <template>
-$END$
+
 </template>
 
-<script>
+<script lang="ts">
+import {onMounted, ref} from 'vue';
+import axios from 'axios';
+
 export default {
-name: "Seasnails.vue"
+  name: "Seasnails.vue",
+  setup(){
+    const seasnails = ref([]);
+    onMounted( async () => {
+      const {data} = await axios.get('seasnails');
+
+      seasnails.value = data
+    });
+    return {
+      seasnails
+    }
+  }
 }
 </script>
 
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import Seasnails from '@/pages/seasnails/Seasnails.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,10 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      children: [
+        {path: 'seasnals', component: Seasnails},
+      ]
     },
     {
       path: '/about',
