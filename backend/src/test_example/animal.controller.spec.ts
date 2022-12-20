8c94f020307581f2e5ac67a981b6231dce882b77
Ryno95 <rmeiboom@student.codam.nl>
2022-12-20 12:17:22 +0100

    added example of with seeding

--- backend/src/test_example/animal.controller.spec.ts
+++ backend/src/test_example/animal.controller.spec.ts
@@ -7,6 +7,8 @@ describe('AnimalController', () => {
   let controller: AnimalController;
   let testingModule: TestingModule;
 
+  const testAnimals = ["Pikkewyn", "Renoster" ,"Kameelperd"];
+
   beforeEach(async () => {
       testingModule = await Test.createTestingModule({
       imports: [...TypeORmTestingModule()],
@@ -15,53 +17,37 @@ describe('AnimalController', () => {
     }).compile();
 
     controller = testingModule.get<AnimalController>(AnimalController);
-  });
 
-  it('Post 3 Animals',async () => {
-    const animals = ["Pikkewyn", "Renoster" ,"Kameelperd"];
-    let i: number = 1;
-    for (const animal in animals)
-    {
-      const newUser = await controller.create({name: animal});
-      expect(newUser.name).toBe(animal);
-      expect(newUser.id).toBe(i);
-      ++i;
+    for (const animal in testAnimals) {
+      await controller.create({name: testAnimals[animal]})
     }
-  })
-
-  it('Count empty db', async () => {
-    const list = await controller.findAll();
-    expect(list).toHaveLength(0);
   });
 
-
-  it('FindAll db with three things', async () => {
-    const animals = ["Pikkewyn", "Renoster" ,"Kameelperd"];
-    for (const animal in animals){ await controller.create({name: animal}); }
-      
+  
+  it('Get all seed animals', async () => {
     const list = await controller.findAll();
     expect(list).toHaveLength(3);
   });
   
+  it('Post new Animals',async () => {
+    const newAnimal = "Sonbeesie";
+    const newAnimalId = 4;
+    const createdAnimal = await controller.create({name: newAnimal});
+    expect(createdAnimal.name).toBe(newAnimal);
+    expect(createdAnimal.id).toBe(newAnimalId);
+  })
   
   it("Update item",async () => {
-      const originalAnimal: string = "Sonbeesie";
       const updatedAnimal: string = "Seekat";
 
-      await controller.create({name: originalAnimal});
-      expect((await controller.getOne(1)).name).toBe(originalAnimal);
-      
       await controller.update(1, {name: updatedAnimal});
       expect((await controller.getOne(1)).name).toBe(updatedAnimal);
   })
 
   it("Delete items from db",async () => {
-    const animals: string[] = ["Pikkewyn", "Renoster" ,"Kameelperd"];
-    for (const animal in animals){ await controller.create({name: animal});}
     let repoOfAnimals = await controller.findAll();
-    expect(repoOfAnimals).toHaveLength(3);
     
-    for (let i = 0; i < animals.length; i++) {await controller.remove(i + 1);}
+    for (let i = 0; i < repoOfAnimals.length; i++) {await controller.remove(i + 1);}
     let shouldBeEmptyArray = await controller.findAll();
     expect(shouldBeEmptyArray).toHaveLength(0);
   })
