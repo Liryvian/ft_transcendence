91efbb8aa0186de06c34a143d67e60f72c4ffb78
Vincent <1089484+vincentvis@users.noreply.github.com>
2022-12-21 12:22:47 +0100

    Vi/typeorm db connection (#34)
    * configure prettier to use tabs
    
    * try connecting to DB
    
    * newline at end of file
    
    * fixes:
    - give the backend container access to the .env file
    
    * use `ci` instad of `i` (install) for installing in the container, this uses just the package-lock.json file and installs the whole project
    
    * add packages to the package.json file
    - typeorm & @nestjs/typeorm
    - pg & @types/pg (postgresql)
    - @nestjs/config
    
    * convert to tabs
    
    * add config service and config module to app.module.ts and tyeporm service
    
    * spaces to tabs
    
    * add exposed port for database for jetbrains internal db management tools (maybe also for vscode?)
    set stats_temp_directory to /tmp to shut up annoying "no permissions" warning
    
    * remove unused ENV variable
    
    * change npm test script to set env variables
    this makes it not rely on a .env file
    and ensures we have test values for the env variables instead of the real ones
    
    * use dotenv for testing in `npm run test` thanks ot @Ryno95
    
    * try to store the values in a variable with await first (making the block async)
    and listing the directory contents of ./backend to see if .env exists and /test/ dir content is ok
    
    * directory contents seemed fine!
    try listing env variables
    try setting env variables hardcoded
    
    * this works, but is very sketchy, and basically the same as just setting this in the package.json file
    
    try logging in dotenv-config.ts
    try cat'ing the contents of env file
    
    * use a dotenv implementation from github marketplace:
    https://github.com/marketplace/actions/simple-dotenv
    
    * different syntax for dotenv action
    
    * try backslack after path?
    
    remove console log
    
    * work dir is set already, so no backend needed in cat
    
    * wtf why is it giving an ecnrypted env file..?
    
    * .env files are encrypted by default! (see .gitattributes)
    naming it `.env.test` should not match the pattern in gitattributes and give us an unencrypted version in the gh action container
    
    * IT WORKS (for backend unit tests)
    
    next step: e2e tests
    
    * get the version of sqlite 3 that might be installed on gh actions
    
    * moved sqlite3 checks, put them in the wrong spot
    
    * where command not found
    
    * use .env.test for tests and better-sqlite3 for testing database
    
    * remove pre-db checks
    
    * some changes to docker
    but still instlaling with `npm install x` does not trigger the install in the container...
    
    * add --silent back to npm ci
    
    Co-authored-by: Vincent <Codam>
    Co-authored-by: Ryno <58690325+Ryno95@users.noreply.github.com>
    Co-authored-by: Vincent <>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './typeorm/typeorm.service';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
---
+++ backend/src/typeorm/typeorm.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+	@Inject(ConfigService)
+	private readonly config: ConfigService;
+
+	public createTypeOrmOptions(): TypeOrmModuleOptions {
+		const testingConfig: TypeOrmModuleOptions = {
+			type: 'better-sqlite3',
+			database: ':memory:'
+		};
+
+		const productionConfig: TypeOrmModuleOptions = {
+			type: 'postgres',
+			host: this.config.get<string>('POSTGRES_SERVER'),
+			port: this.config.get<number>('POSTGRES_PORT'),
+			database: this.config.get<string>('POSTGRES_DB'),
+			username: this.config.get<string>('POSTGRES_USER'),
+			password: this.config.get<string>('POSTGRES_PASSWORD'),
+			autoLoadEntities: true, // do not have this turned on for production!
+			// entities: ['dist/**/*.entity.{ts,js}'],
+			// migrations: ['dist/migrations/*.{ts,js}'],
+			// migrationsTableName: 'typeorm_migrations',
+			logger: 'file',
+			synchronize: true, // never use TRUE in production!
+		};
+
+		if (this.config.get<string>('USE_TEST_DB') == "1") {
+			return testingConfig;
+		}
+
+		return productionConfig;
+	}
+}
