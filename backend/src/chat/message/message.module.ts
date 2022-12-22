4d3230d2a7d4cf65b6cb2fff69ace065c364933f
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-12-22 16:55:34 +0100

    auth added

--- backend/src/chat/message/message.module.ts
+++ backend/src/chat/message/message.module.ts
@@ -3,10 +3,12 @@ import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Message])],
-  controllers: [MessageController],
+  imports: [TypeOrmModule.forFeature([Message]), AuthModule],
+  controllers: [MessageController, AuthController],
   providers: [MessageService]
 })
 export class MessageModule {}
