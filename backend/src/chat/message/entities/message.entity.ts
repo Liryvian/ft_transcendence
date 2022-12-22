af17aac82fce3db22c977d88a9b098c46520bb29
Lisa Vlamings <livlamin@f0r3s22.codam.nl>
2022-12-22 17:30:50 +0100

    primary key officaly added to message.entity

--- backend/src/chat/message/entities/message.entity.ts
+++ backend/src/chat/message/entities/message.entity.ts
@@ -3,11 +3,8 @@ import { Chatroom } from '../../chatroom/entities/chatroom.entity';
 
 @Entity('messages')
 export class Message {
-  @PrimaryGeneratedColumn() // we didn't designed this but my programme said: Primary column is required to have in all your entities. Use @PrimaryColumn decorator to add a primary column to your entity.
+  @PrimaryGeneratedColumn()
   id: number;
-
-  // @Column() // should this be a primary key?
-  // id: string;
 
   @Column()
   timestamp: number;
