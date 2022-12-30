fb2ed4a425c4416a0db106ed23453073a9c7e2c7
Vincent <Codam>
2022-12-30 17:54:17 +0100

    fixes circular dependency in testing

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
@@ -7,7 +7,11 @@ import { AuthModule } from '../auth/auth.module';
 import { SharedModule } from '../shared/shared.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User]), AuthModule, SharedModule],
+	imports: [
+		TypeOrmModule.forFeature([User]),
+		forwardRef(() => AuthModule),
+		SharedModule,
+	],
 	controllers: [UserController],
 	providers: [UserService],
 	exports: [UserService],
