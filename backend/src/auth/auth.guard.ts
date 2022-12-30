461ade246cf9283fba5ce659d50fd14d4ebc8f57
Vincent <Codam>
2022-12-30 11:34:07 +0100

    use return type from auto generated guard (so it can be `boolean | promise | observable`)

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -1,14 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 	constructor(private jwtService: JwtService) {}
-	canActivate(context: ExecutionContext) {
+	canActivate(
+		context: ExecutionContext,
+	): boolean | Promise<boolean> | Observable<boolean> {
 		const request = context.switchToHttp().getRequest();
 		const jwt = request.cookies['jwt'];
 		try {
-			return this.jwtService.verify(jwt);
+			const validated = this.jwtService.verify(jwt);
+			return validated;
 		} catch (e) {
 			return false;
 		}
