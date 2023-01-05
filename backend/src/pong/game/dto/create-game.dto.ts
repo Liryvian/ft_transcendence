9decb63c651da62936c26b1f971187cb8a816350
Ryno95 <rmeiboom@student.codam.nl>
2023-01-05 18:12:30 +0100

    added custom DoesNotMatch decorator

--- backend/src/pong/game/dto/create-game.dto.ts
+++ backend/src/pong/game/dto/create-game.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty } from 'class-validator';
+import { DoesNotMatch } from 'src/shared/does-not-match.decorator';
 
 export class CreateGameDto {
 	customization?: string;
@@ -6,6 +7,7 @@ export class CreateGameDto {
 	@IsNotEmpty()
 	player_one: number;
 
+	@DoesNotMatch(CreateGameDto, (o) => o.player_one)
 	@IsNotEmpty()
 	player_two: number;
 }
