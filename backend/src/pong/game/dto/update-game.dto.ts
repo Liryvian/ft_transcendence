7e76b079d0c5b335ee51b9460e69e365d20fb3f0
Ryno95 <rmeiboom@student.codam.nl>
2023-01-05 18:12:49 +0100

    removed unused imports

--- backend/src/pong/game/dto/update-game.dto.ts
+++ backend/src/pong/game/dto/update-game.dto.ts
@@ -1,6 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGameDto } from './create-game.dto';
-
 export class UpdateGameDto {
 	score_player_one?: number;
 	score_player_two?: number;
