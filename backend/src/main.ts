0495147ca2101a5b3324b1274fb707632a74d1a8
Vincent <>
2023-01-10 10:12:23 +0100

    move validation pipe config to a file so we can access it from the tests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,6 @@ import { globalValidationPipeOptions } from './main.validationpipe';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	/*
-		whitelist: strips property values that do not exist in the expected DTO (DTO acts as a whitelist)
-		transform: transforms the plain js object to the actual DTO class (performs string to number conversions etc)
-		forbidNonWhitelisted: throw an error if any non-whitelisted property is present in the incoming object (<--- do we want this?)
-	*/
 	app.useGlobalPipes(new ValidationPipe(globalValidationPipeOptions()));
 	await app.listen(3000);
 }
