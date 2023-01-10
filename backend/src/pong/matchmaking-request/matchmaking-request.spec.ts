09dbf733273becaeb9328e2beb438de90a2a71ca
Ryno95 <rmeiboom@student.codam.nl>
2023-01-10 14:59:46 +0100

    added test files with basic test

--- backend/src/pong/matchmaking-request/matchmaking-request.spec.ts
+++ backend/src/pong/matchmaking-request/matchmaking-request.spec.ts
@@ -1,18 +1,34 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from '../../typeorm/typeorm.service';
+import { MatchmakingRequest } from './entities/matchmaking-request.entity';
+import { MatchmakingRequestController } from './matchmaking-request.controller';
 import { MatchmakingRequestService } from './matchmaking-request.service';
 
 describe('MatchmakingRequestService', () => {
-  let service: MatchmakingRequestService;
+	let service: MatchmakingRequestService;
+	let controller: MatchmakingRequestController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MatchmakingRequestService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [
+				ConfigModule.forRoot({ isGlobal: true }),
+				TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+				TypeOrmModule.forFeature([MatchmakingRequest]),
+			],
+			providers: [MatchmakingRequestService],
+			controllers: [MatchmakingRequestController],
+		}).compile();
 
-    service = module.get<MatchmakingRequestService>(MatchmakingRequestService);
-  });
+		service = module.get<MatchmakingRequestService>(MatchmakingRequestService);
+		controller = module.get<MatchmakingRequestController>(
+			MatchmakingRequestController,
+		);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+		expect(controller).toBeDefined();
+	});
 });
