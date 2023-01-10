9caea8ba0e9db1823606b7ce3816ea718e23d652
Vincent <Codam>
2023-01-10 13:42:26 +0100

    remove class-validator decorator from entity definition
    change baseUrl to something that sometimes works a bit better

--- backend/src/test_example/entities/animals.entity.ts
+++ backend/src/test_example/entities/animals.entity.ts
@@ -1,4 +1,3 @@
-import { IsNotEmpty } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('animals')
@@ -7,6 +6,5 @@ export class AnimalEntity {
 	id: number;
 
 	@Column({ unique: true })
-	@IsNotEmpty()
 	name: string;
 }
