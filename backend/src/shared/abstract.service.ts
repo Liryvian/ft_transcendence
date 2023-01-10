c0a3a4bee24535aefe39efc4bf3cf9e25be2d59f
Vincent <>
2023-01-10 10:14:35 +0100

    small update to abstract service
    - avoid extra call to DB when doing update, but use update result to throw NotFoundException
    - allow array of numbers for remove (makes bulk remove possible)
    - add a check for typeorm throwing when doing `repository.delete(null)` or `repository.delete([]`
    - add some comments for future use

--- backend/src/shared/abstract.service.ts
+++ backend/src/shared/abstract.service.ts
@@ -16,6 +16,11 @@ export abstract class AbstractService<T> {
 		return this.repository.find(condition);
 	}
 
+	/*
+		the insert method allows you to insert either:
+		- a single entity
+		- an array of entitites
+	*/
 	async create(data): Promise<InsertResult> {
 		return this.repository.insert(data);
 	}
@@ -30,11 +35,25 @@ export abstract class AbstractService<T> {
 	}
 
 	async update(id: number, data): Promise<UpdateResult> {
-		await this.findOne({ where: { id } });
-		return this.repository.update(id, data);
+		const updateResult: UpdateResult = await this.repository.update(id, data);
+		if (updateResult.affected === 0) {
+			throw new NotFoundException();
+		}
+		return updateResult;
 	}
 
 	async remove(id: number | number[]): Promise<DeleteResult> {
+		// you can give an empty object/array, or NULL to this function
+		// which will cause TypeORM to throw an error
+		// in TS/JS NULL is an object, so we can check for it quite easily
+		if (typeof id === 'object') {
+			if (id === null || (id.hasOwnProperty('length') && id.length === 0)) {
+				const res: DeleteResult = new DeleteResult();
+				res.raw = [];
+				res.affected = 0;
+				return res;
+			}
+		}
 		return this.repository.delete(id);
 	}
 }
