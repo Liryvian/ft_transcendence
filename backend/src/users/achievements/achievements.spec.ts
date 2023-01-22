ecbff58c7234be7d8b7fb746ad3447313c9f5d07
Rynoster <Ryno.Meiboom@gmail.com>
2023-01-22 12:46:49 +0100

    hehe imports :.. )

--- backend/src/users/achievements/achievements.spec.ts
+++ backend/src/users/achievements/achievements.spec.ts
@@ -1,21 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AllTestingModule } from '../../shared/test.module';
 import { AchievementsController } from './achievements.controller';
 import { AchievementsService } from './achievements.service';
 
 describe('AchievementsService', () => {
-  let service: AchievementsService;
-  let controller: AchievementsController;
+	let service: AchievementsService;
+	let controller: AchievementsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AchievementsService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [AllTestingModule],
+		}).compile();
 
-    service = module.get<AchievementsService>(AchievementsService);
-    controller = module.get<AchievementsController>(AchievementsController);
-  });
+		service = module.get<AchievementsService>(AchievementsService);
+		controller = module.get<AchievementsController>(AchievementsController);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
