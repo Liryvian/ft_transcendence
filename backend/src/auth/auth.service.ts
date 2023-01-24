b73de2f9588f8dab72f3c6097320fae84d6a596f
Vincent <>
2023-01-24 19:35:00 +0100

    move actual loggin logic in to service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { Request } from 'express';
+import { Response, Request } from 'express';
 
 @Injectable()
 export class AuthService {
 	constructor(private jwtService: JwtService) {}
+
+	async login(id: number, response: Response) {
+		const jwt = await this.jwtService.signAsync({ id: id });
+		response.cookie('jwt', jwt, { httpOnly: true });
+	}
 
 	async userId(request: Request): Promise<number> {
 		const cookie = request.cookies['jwt'];
