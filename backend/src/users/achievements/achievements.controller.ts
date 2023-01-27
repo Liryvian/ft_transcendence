f2947b44d9000f37c894baddb71b52886f7b1246
Ryno95 <rmeiboom@student.codam.nl>
2023-01-27 12:19:37 +0100

    create() => insert()

--- backend/src/users/achievements/achievements.controller.ts
+++ backend/src/users/achievements/achievements.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+} from '@nestjs/common';
 import { AchievementsService } from './achievements.service';
 import { CreateAchievementDto } from './dto/create-achievement.dto';
 import { UpdateAchievementDto } from './dto/update-achievement.dto';
 
 @Controller('achievements')
 export class AchievementsController {
-  constructor(private readonly achievementsService: AchievementsService) {}
+	constructor(private readonly achievementsService: AchievementsService) {}
 
-  @Post()
-  create(@Body() createAchievementDto: CreateAchievementDto) {
-    return this.achievementsService.create(createAchievementDto);
-  }
+	@Post()
+	create(@Body() createAchievementDto: CreateAchievementDto) {
+		return this.achievementsService.insert(createAchievementDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.achievementsService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.achievementsService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.achievementsService.findOne({where: {id}});
-  }
+	@Get(':id')
+	findOne(@Param('id') id: number) {
+		return this.achievementsService.findOne({ where: { id } });
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateAchievementDto: UpdateAchievementDto) {
-    return this.achievementsService.update(id, updateAchievementDto);
-  }
+	@Patch(':id')
+	update(
+		@Param('id') id: number,
+		@Body() updateAchievementDto: UpdateAchievementDto,
+	) {
+		return this.achievementsService.update(id, updateAchievementDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.achievementsService.remove(id);
-  }
+	@Delete(':id')
+	remove(@Param('id') id: number) {
+		return this.achievementsService.remove(id);
+	}
 }
