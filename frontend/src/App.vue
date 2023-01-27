bc97d4eac4f8580028dbbce9ac7af3b29d80b498
Vincent <>
2023-01-27 10:23:13 +0100

    it's a mess!

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,83 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+	<header>
+		<nav>
+			<div class="nav_ball"></div>
+			<RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+			<RouterLink to="/about">About</RouterLink>
+		</nav>
+	</header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+	<RouterView />
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+	width: 100vw;
+	border-bottom: var(--border-width) solid var(--color-border);
+	padding: 0.2em;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+	position: relative;
+}
+
+.nav_ball {
+	display: block;
+	position: absolute;
+	width: var(--ball-size);
+	height: var(--ball-size);
+	border: var(--border-width) solid var(--color-border);
+	border-radius: 50%;
+	top: 50%;
+	transform: translateY(-50%);
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+	color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+	background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+	display: inline-block;
+	padding: 1em 2em;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
+/* nav a:first-of-type {
+	border: 0;
+} */
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+	header {
+		/* display: flex; */
+		/* place-items: center; */
+		/* padding-right: calc(var(--section-gap) / 2); */
+	}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+	header .wrapper {
+		/* display: flex; */
+		/* place-items: flex-start; */
+		/* flex-wrap: wrap; */
+	}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+	nav {
+		/* text-align: left; */
+		/* margin-left: -1rem; */
+		/* font-size: 1rem; */
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+		/* padding: 1rem 0; */
+		/* margin-top: 1rem; */
+	}
 }
 </style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView,
+		},
+		{
+			path: '/about',
+			name: 'about',
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import('../views/AboutView.vue'),
+		},
+	],
+});
 
-export default router
+router.afterEach((to, from, failure) => {
+	console.log({ to, from, failure });
+});
+
+export default router;
