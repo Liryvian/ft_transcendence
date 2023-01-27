43f9f177583f2a35bb67d3f4f9dac6b2dd557553
Vincent <1089484+vincentvis@users.noreply.github.com>
2023-01-27 14:31:13 +0100

    Vi/feat intra42login (#93)
    * add tests + endpoints for first steps in intra auth flow
    
    * remove unused imports
    
    * it's going somehwere
    
    * add fields to user
    update controller methods to work better + TODO comments
    
    * little steps
    
    * logging + remove unused imports
    
    * soo this mess of code creates a user the first time and gets the existing user after that..
    
    refactor time?
    
    * write test todos
    
    * fix user entity additions to allow empty intra values
    
    * tsconfig is annoying af..
    
    * auth
    
    * add intra_login as separate field (non-changable..)
    
    * more working tests
    
    * move actual loggin logic in to service
    
    * move some stuff to authService
    
    * getting somewhere
    
    refactored to make single functions semi-testable (most of it is api calls, so not really a need to test... just some "what if api call fails I guess")
    moved more functions to service so only endpoints stay in controller
    
    * refactor tests
    refactor username selection
    
    * make sure tests for displayname are complete
    
    try something else with state (hashed config variable and not set it in the cookie, so everything can happen in the guard for checking ?)
    
    * more guard tries
    
    * inra42 api register/login ready for review
    
    * use separate env paramter
    
    * add checks around api call
    
    * remove empty function
    
    * rename to insert
    
    * remove state
    fix to use InsertResult
    
    Co-authored-by: Vincent <Codam>
    Co-authored-by: Vincent <>

--- backend/src/auth/auth.spec.ts
+++ backend/src/auth/auth.spec.ts
@@ -4,11 +4,17 @@ import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
 
 import { AllTestingModule } from '../shared/test.module';
+import { IntraTokendataDto } from './dto/intra-tokendata.dto';
+import { UserService } from '../users/user/user.service';
+import { ILike } from 'typeorm';
+import { User } from '../users/user/entities/user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('Auth', () => {
 	let authController: AuthController;
 	let authService: AuthService;
 	let authGuard: AuthGuard;
+	let userService: UserService;
 
 	beforeAll(async () => {
 		const module: TestingModule = await Test.createTestingModule({
@@ -18,6 +24,7 @@ describe('Auth', () => {
 		authController = module.get<AuthController>(AuthController);
 		authService = module.get<AuthService>(AuthService);
 		authGuard = module.get<AuthGuard>(AuthGuard);
+		userService = module.get<UserService>(UserService);
 	});
 
 	afterAll(() => {});
@@ -37,6 +44,153 @@ describe('Auth', () => {
 	describe('AuthService', () => {
 		it('should be defined', () => {
 			expect(authService).toBeDefined();
+		});
+	});
+
+	describe('Api register/login flow', () => {
+		const fakeUserData = {
+			id: 22346,
+			login: 'fakeintrauser',
+		};
+		const fakeTokenData: IntraTokendataDto = {
+			token_type: 'bearer',
+			access_token: 'access_token',
+			refresh_token: 'refresh_token',
+			scope: 'scope',
+			expires_in: 1000,
+			created_at: 1706101614,
+		};
+
+		describe('for existing user', () => {
+			it('should redirect to dashboard and not update name', async () => {
+				await userService.insert({
+					name: 'not_fakeintrauser',
+					intra_id: fakeUserData.id,
+				});
+
+				const { redirectLocation, userId } = await authService.processUserData(
+					fakeUserData,
+				);
+				expect(redirectLocation).toContain('recurring_user');
+				const users = await userService.findAll({
+					where: { intra_id: fakeUserData.id },
+				});
+				expect(users).toHaveLength(1);
+				expect(users[0].name).toBe('not_fakeintrauser');
+				expect(users[0].id).toBe(userId);
+				await userService.remove(users[0].id);
+			});
+		});
+
+		describe('for new user', () => {
+			it('should create a new user and return a redirect to profile', async () => {
+				const { redirectLocation, userId } = await authService.processUserData(
+					fakeUserData,
+				);
+				expect(redirectLocation).toContain('new_user');
+				const users = await userService.findAll({
+					where: { intra_id: fakeUserData.id },
+				});
+				expect(users).toHaveLength(1);
+				expect(users[0].name).toBe(fakeUserData.login);
+				await userService.remove(users[0].id);
+			});
+		});
+
+		describe('when other user with same name as new user intra_login exists', () => {
+			it('should add a suffix to the new users displayname', async () => {
+				const u0 = await userService.insert({
+					name: fakeUserData.login,
+					password: 'abc',
+					is_intra: false,
+				});
+
+				// create api user with normal user name collision
+				// name should be prefixed with intra id
+				const u1 = await authService.processUserData(fakeUserData);
+
+				// create user with same username but different id
+				const u2 = await authService.processUserData({
+					...fakeUserData,
+					id: 2091,
+				});
+
+				// create a 'non-intra' user with the username
+				// that would be generated if it was an intra user...
+				const u3 = await userService.insert({
+					name: fakeUserData.login + '_2091_4894',
+					password: 'p',
+					is_intra: false,
+				});
+
+				// make a collision on login name + id (user from above)
+				// should generate random bytes after name
+				const u4 = await authService.processUserData({
+					login: 'fakeintrauser_2091',
+					id: 4894,
+				});
+
+				// create an expected object
+				const expectation = [
+					{
+						name: fakeUserData.login,
+						id: u0.identifiers[0].id,
+					},
+					{
+						name: fakeUserData.login + '_22346',
+						id: u1.userId,
+					},
+					{
+						name: fakeUserData.login + '_2091',
+						id: u2.userId,
+					},
+					{
+						name: fakeUserData.login + '_2091_4894',
+						id: u3.identifiers[0].id,
+					},
+					{
+						name: expect.stringMatching(
+							/^fakeintrauser_2091_4894_[a-fA-F0-9]{1,8}$/,
+						),
+						id: u4.userId,
+					},
+				];
+
+				const usersMatchingUsername: User[] = await userService.findAll({
+					where: { name: ILike('fakeintrauser%') },
+				});
+				expect(usersMatchingUsername).toHaveLength(5);
+
+				const dataToVerify = usersMatchingUsername.map((user: User) => ({
+					name: user.name,
+					id: user.id,
+				}));
+				expect(dataToVerify).toEqual(expectation);
+			});
+		});
+
+		describe('validation of intra token response', () => {
+			it('should validate a complete object', async () => {
+				const validated = await authService.validateIntraTokenData(
+					fakeTokenData,
+				);
+				expect(validated).toEqual(fakeTokenData);
+			});
+
+			it('should throw on incomplete object', async () => {
+				const incomplete = {
+					token_type: 'bearer',
+					refresh_token: 'refresh_token',
+					scope: 'scope',
+					expires_in: 1000,
+					created_at: 1706101614,
+				};
+				try {
+					await authService.validateIntraTokenData(incomplete);
+				} catch (e) {
+					expect(e).toBeInstanceOf(BadRequestException);
+				}
+			});
 		});
 	});
 });
