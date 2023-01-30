28cd88e0935512ca47ba593a851d9eaf1c5d40af
Rynoster <Ryno.Meiboom@gmail.com>
2023-01-30 13:28:34 +0100

    stash

--- frontend/src/components/ProfileList.vue
+++ frontend/src/components/ProfileList.vue
@@ -1,19 +1,14 @@
 <template>
    <div class="table-responsive">
   <table class="table table-striped table-sm">
-    <thead>
-    <tr>
-      <th>#</th>
-      <th>Name</th>
-      <!-- <th>Email</th> -->
-      <!-- <th>Role</th> -->
-      <!-- <th>Action</th> -->
-    </tr>
-    </thead>
     <tbody>
     <tr v-for="user in allUsers" :key="user.id">
+      <td><img src="../../public/favicon-32x32.png" alt="Avatar" class="avatar"></td>
       <td>{{ user.id }}</td>
       <td>{{ user.name }} </td>
+      <td> <button>Chat</button> </td>
+      <td> <button>Add as friend</button> </td>
+      <td> <button>Block</button> </td>
       <td></td>
     </tr>
     </tbody>
@@ -23,38 +18,62 @@
   </template>
 
 <script lang="ts">
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
+import { onMounted } from 'vue';
+import { defineComponent } from "vue";
 
-    export default {
+type User = {
+  name: string;
+  id: number;
+}
+const defautAvatar: string = '../../public/favicon-32x32.png';
+
+let allUsers: User[] = [];
+  export default defineComponent({
     name: 'ProfileList',
-
-    setup() {
-        const allUsers = ref([]);
-        
-        onMounted(async () => {
-            const { data } = await axios.get('http://localhost:8080/api/users');
-            allUsers.value = data.data;
-        });
-        return allUsers;
+    data(){
+      return {allUsers};
     },
- }
+    async mounted()
+    {
+      const response = await fetch('http://localhost:8080/api/users');
+          allUsers = await response.json();
+          allUsers.sort((a: User, b: User) => a.id - b.id)
+          return {allUsers};
+    }, 
+    // setup() {
+        
+    //     onMounted(async () => {
+    //       const response = await fetch('http://localhost:8080/api/users');
+    //       allUsers = await response.json();
+    //       allUsers.sort((a: User, b: User) => a.id - b.id)
+    //       // allUsers.forEach(user => {
+    //       //   if (!user.avatar) {
+    //       //     user.avatar = defautAvatar;
+    //       //   }
+    //       // })
+    //     }
+    //     );
+    //     return {allUsers};
+    //   },
+ })
 </script>
 
 
-  <style>
+ <style>
+ .avatar {
+  vertical-align: middle;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+button {
+  all: unset;
+  cursor: pointer;
+}
+
+button:focus {
+  outline: blue 5px auto;
+}
 </style>
 
-<!-- setup() {
-    const users = ref([]);
-    };
-    onMounted(async () => {
-      const {data} = await axios.get('users');
-      users.value = data.data;
-      }
-    }
-    return {
-      users,
-    }
-  }
-} -->
--- frontend/src/views/ProfilesView.vue
+++ frontend/src/views/ProfilesView.vue
@@ -1,7 +1,19 @@
 <template>
 	<div class="page_box_wrapper">
 		<div class="page_box">
-			<h1>This is a profiles page</h1>
+			<h1>Profiles</h1>
+
+			<ProfileList />
 		</div>
 	</div>
 </template>
+
+<script lang="ts">
+import ProfileList from '@/components/ProfileList.vue';
+
+export default {
+	components: {
+		ProfileList
+	}
+}
+</script>
