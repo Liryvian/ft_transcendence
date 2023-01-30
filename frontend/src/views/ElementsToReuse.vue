4f51a8a4a37bba018a2459c37e5a6683dea648fc
Vincent <>
2023-01-30 13:09:53 +0100

    Add base for elements add form input field

---
+++ frontend/src/views/ElementsToReuse.vue
@@ -0,0 +1,23 @@
+<!-- FILE LATER TO BE REMOVED -->
+
+<template>
+    <div class="page_box_wrapper">
+		<div class="page_box">
+
+			<h1>Some Elements to reuse</h1>
+
+            <div class="form_group">
+                <div class="field_group">
+                    <label for="">field label:</label>
+                    <input type="text" value="disabled field" disabled>
+                </div>
+
+                <div class="field_group">
+                    <label for="">field label:</label>
+                    <input type="text" placeholder="placeholder text (optional)">
+                </div>
+            </div>
+
+		</div>
+	</div>
+</template>
