3ee6265cda70d4f9394381004639f927ebab1fae
Vincent <>
2023-01-31 09:49:56 +0100

    a button

--- frontend/src/views/ElementsToReuse.vue
+++ frontend/src/views/ElementsToReuse.vue
@@ -6,6 +6,7 @@
 
             <h1>Some Elements to reuse</h1>
 
+            <!-- Without pb_bottom it's positioned on the top -->
             <div class="page_button pb_bottom">
                 <a href="#">save</a>
             </div>
