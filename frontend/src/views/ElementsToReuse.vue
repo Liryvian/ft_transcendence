0442d9cd9336272baa3296bb1b6b0e71086a7220
Vincent <Codam>
2023-02-02 15:52:23 +0100

    more elements

--- frontend/src/views/ElementsToReuse.vue
+++ frontend/src/views/ElementsToReuse.vue
@@ -2,6 +2,8 @@
 
 <template>
 	<div class="page_box_wrapper">
+		<div class="c_game__name">NAME</div>
+		<div class="c_game__name c_game__name--right">NAME</div>
 		<div class="page_box">
 			<h1>Some Elements to reuse</h1>
 
@@ -11,7 +13,7 @@
 			</div>
 
 			<div class="form_group">
-				<div class="field_group">
+				<div class="c_field_group">
 					<label for="disabled_field">field label:</label>
 					<input
 						id="disabled_field"
@@ -21,13 +23,35 @@
 					/>
 				</div>
 
-				<div class="field_group">
+				<div class="c_field_group">
 					<label for="example_field">field label:</label>
 					<input
 						id="example_field"
 						type="text"
 						placeholder="placeholder text (optional)"
 					/>
+				</div>
+
+				<div class="c_field_group c_field_group--radio">
+					<div class="c_radio_group">
+						<label for="example_radio2">public</label>
+						<input
+							name="same"
+							id="example_radio2"
+							type="radio"
+							value="on"
+							checked
+						/>
+					</div>
+					<div class="c_radio_group">
+						<label for="example_radio1">private</label>
+						<input
+							name="same"
+							id="example_radio1"
+							type="radio"
+							value="on"
+						/>
+					</div>
 				</div>
 			</div>
 
@@ -45,6 +69,31 @@
 					<div class="c_media__asset c_asset--offline">
 						<div class="c_asset__circle">
 							<img src="/test-profile.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+				<div class="c_media c_media--clickable">
+					<div class="c_media__asset c_asset--multi">
+						<div class="c_asset c_asset__circle">
+							<img src="/vaalboskat.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/renoster.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+				<div class="c_media c_media--clickable">
+					<div class="c_media__asset c_asset--multi">
+						<div class="c_asset c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/vaalboskat.png" alt="" />
 						</div>
 					</div>
 					<div class="c_media__content">some piece of text</div>
