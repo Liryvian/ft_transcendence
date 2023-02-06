7b9d063956f54584171667286bcfc80b96ef584a
Vincent <1089484+vincentvis@users.noreply.github.com>
2023-02-06 13:55:56 +0100

    Vi/frontend elements (#110)
    * it's a mess!
    
    * nav
    
    * base views for the box in which the content will live
    
    * fix box
    
    * fix some css defaults
    remove components (cleaning)
    
    * update compose to have public folder
    
    * fix prettier config
    
    * add favicon
    
    * add fonts
    
    * hopefully this is enough for testing
    
    * Add base for elements
    add form input field
    
    * change aspect ratio of main content container
    
    * remove unused wrapper element
    
    * add back wrapper for absolute positioning element that goes over the boundaries
    
    * a button
    
    * base for media elem
    
    * add online-offline status
    some restructuring
    
    * more elements
    
    * some chat
    
    * align in center again
    
    * almost some chat on small screens
    
    * chat
    
    * change to use writing-mode
    
    * some small fixes
    and added a split element
    
    * should hide scrollbars when not needed and remove horizontal scroll view
    make textrea non-resizeable
    
    ---------
    
    Co-authored-by: Vincent <>
    Co-authored-by: Vincent <Codam>

---
+++ frontend/src/views/ElementsToReuse.vue
@@ -0,0 +1,109 @@
+<!-- FILE LATER TO BE REMOVED -->
+
+<template>
+	<div class="page_box_wrapper">
+		<div class="c_game__name">NAME</div>
+		<div class="c_game__name c_game__name--right">NAME</div>
+		<div class="page_box">
+			<h1>Some Elements to reuse</h1>
+
+			<!-- Without pb_bottom it's positioned on the top -->
+			<div class="page_button pb_bottom">
+				<a href="#">save</a>
+			</div>
+
+			<div class="c_block c_form_group">
+				<div class="c_field_group">
+					<label for="disabled_field">field label:</label>
+					<input
+						id="disabled_field"
+						type="text"
+						value="disabled field"
+						disabled
+					/>
+				</div>
+
+				<div class="c_field_group">
+					<label for="example_field">field label:</label>
+					<input
+						id="example_field"
+						type="text"
+						placeholder="placeholder text (optional)"
+					/>
+				</div>
+
+				<div class="c_field_group c_field_group--radio">
+					<div class="c_radio_group">
+						<label for="example_radio2">public</label>
+						<input
+							name="same"
+							id="example_radio2"
+							type="radio"
+							value="on"
+							checked
+						/>
+					</div>
+					<div class="c_radio_group">
+						<label for="example_radio1">private</label>
+						<input
+							name="same"
+							id="example_radio1"
+							type="radio"
+							value="on"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div class="c_block c_split">
+				<p><a href="#">one</a> / <a href="#">two</a></p>
+				<p><a href="#">three</a></p>
+			</div>
+
+			<div class="c_block c_list">
+				<div class="c_media">
+					<div class="c_media__asset c_asset--online">
+						<div class="c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+
+				<div class="c_media c_media--clickable">
+					<div class="c_media__asset c_asset--offline">
+						<div class="c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+				<div class="c_media c_media--clickable">
+					<div class="c_media__asset c_asset--multi">
+						<div class="c_asset c_asset__circle">
+							<img src="/vaalboskat.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/renoster.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+				<div class="c_media c_media--clickable">
+					<div class="c_media__asset c_asset--multi">
+						<div class="c_asset c_asset__circle">
+							<img src="/test-profile.png" alt="" />
+						</div>
+						<div class="c_asset c_asset__circle">
+							<img src="/vaalboskat.png" alt="" />
+						</div>
+					</div>
+					<div class="c_media__content">some piece of text</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
