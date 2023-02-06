29381380d10ca8567bca9205cec52ea3c6a135e4
Lisa Vlamings <livlamin@f1r6s15.codam.nl>
2023-02-06 14:51:31 +0100

    so far

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -2,6 +2,26 @@
 	<div class="page_box_wrapper">
 		<div class="page_box">
 			<h1>This is a profile page</h1>
+			<OverviewWithMidline/>
+			<HorizontalAvatarAndUserName :data-array="dataArray"/>
 		</div>
 	</div>
 </template>
+
+<script lang="ts">>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+let dataArray = [
+	{ left: 'intra name', right: 'x' },
+	{ left: 'member since', right: 'a' },
+	{ left: 'wins', right: 'a' },
+	{ left: 'losses', right: 'a' },
+	{ left: 'achievements', right: 'a' },
+];
+import OverviewWithMidline from '@/components/overviews/OverviewWithMidline.vue';
+import HorizontalAvatarAndUserName from '@/components/user-info/HorizontalAvatarAndUserName.vue';
+export default {
+	components: { HorizontalAvatarAndUserName, OverviewWithMidline },
+};
+</script>
