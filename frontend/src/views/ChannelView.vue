79c858827fc6af8f817243690477a17abceb0191
Lisa Vlamings <livlamin@f1r6s15.codam.nl>
2023-02-06 13:52:51 +0100

    pages added

---
+++ frontend/src/views/ChannelView.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="page_box_wrapper">
+		<div class="page_box">
+			<h1>This is a channel page</h1> //!!! --> not sure is we get a separate channel vieuw page? as in only one button is different so maybe there are other ways, else, here it is!
+		</div>
+	</div>
+</template>
