9271d627c1059ba3ac24eac1f6fe4ee32ac3bdb7
Ryno <58690325+Ryno95@users.noreply.github.com>
2023-02-06 12:16:22 +0100

    Ry/seeding this farm (#109)
    * seeded users, chat, and games
    
    * import path fix
    
    * added user-relationship seed
    
    * final
    
    * removed chat seeding

--- backend/src/seeders/seeders.module.ts
+++ backend/src/seeders/seeders.module.ts
@@ -1,11 +1,36 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../../src/auth/auth.module';
+import { ChatModule } from '../../src/chats/chat/chat.module';
+import { MessageModule } from '../../src/chats/message/message.module';
+import { GameModule } from '../../src/pong/game/game.module';
+import { GameInvitesModule } from '../../src/pong/game_invite/game-invite.module';
+import { MatchmakingRequestModule } from '../../src/pong/matchmaking-request/matchmaking-request.module';
+import { SharedModule } from '../../src/shared/shared.module';
+import { AchievementsModule } from '../../src/users/achievements/achievements.module';
+import { UserAchievementsModule } from '../../src/users/user-achievements/user-achievements.module';
+import { UserRelationshipModule } from '../../src/users/user-relationship/user-relationship.module';
+import { UserController } from '../../src/users/user/user.controller';
+import { UserModule } from '../../src/users/user/user.module';
 import { AnimalModule } from '../test_example/animal.module';
-import { AnimalEntity } from '../test_example/entities/animals.entity';
 import { SeederService } from './seeders.service';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([AnimalEntity]), AnimalModule],
+	imports: [
+		AnimalModule,
+		AuthModule,
+		SharedModule,
+		ChatModule,
+		MessageModule,
+		UserModule,
+		GameModule,
+		GameInvitesModule,
+		MatchmakingRequestModule,
+		SeedersModule,
+		UserRelationshipModule,
+		AchievementsModule,
+		UserAchievementsModule,
+	],
 	providers: [SeederService],
+	controllers: [UserController],
 })
 export class SeedersModule {}
--- backend/src/seeders/seeders.service.ts
+++ backend/src/seeders/seeders.service.ts
@@ -2,14 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { AnimalService } from '../test_example/animal.service';
 import * as fs from 'fs';
 import seedData from './seed.data';
+import { UserService } from '../users/user/user.service';
+import { GameService } from '../pong/game/game.service';
+import { assert } from 'console';
+import { Game } from '../pong/game/entities/game.entity';
+import { ChatService } from '../chats/chat/chat.service';
+import { User } from '../users/user/entities/user.entity';
+import { UserRelationshipService } from '../users/user-relationship/user-relationship.service';
+import { CreateUserRelationshipDto } from '../users/user-relationship/dto/create-user-relationship.dto';
+import { validRelationships } from '../users/user-relationship/entities/user-relationship.entity';
 
 @Injectable()
 export class SeederService {
-	inject: [AnimalService];
+	inject: [AnimalService, UserService, GameService];
 
-	constructor(private readonly animalService: AnimalService) {}
-
-	private readonly shouldSeedFilePath = './dist/seeders/.hasSeeded';
+	constructor(
+		private readonly animalService: AnimalService,
+		private readonly userService: UserService,
+		private readonly gameService: GameService,
+		private readonly chatService: ChatService,
+		private readonly userRelService: UserRelationshipService,
+	) {}
+	// on my local machine the src doesn't exist in this path
+	private readonly shouldSeedFilePath = './dist/src/seeders/.hasSeeded';
 
 	shouldSeed(): boolean {
 		return fs.existsSync(this.shouldSeedFilePath) === false;
@@ -19,8 +34,63 @@ export class SeederService {
 		fs.writeFileSync(this.shouldSeedFilePath, '');
 	}
 
+	async seedUsers() {
+		// passwords are visible
+		await this.userService.trySeed(seedData.users());
+	}
+
+	async seedGames() {
+		await this.gameService.trySeed(seedData.games());
+		const seededGames: Game[] = await this.gameService.findAll();
+		assert(seededGames.length === 3);
+
+		seededGames[0].score_player_one = 10;
+		seededGames[0].score_player_two = 5;
+		seededGames[0].is_active = false;
+
+		seededGames[1].score_player_one = 8;
+		seededGames[1].score_player_two = 3;
+		seededGames[1].is_active = false;
+
+		seededGames[2].score_player_one = 12;
+		seededGames[2].score_player_two = 7;
+		seededGames[2].is_active = false;
+		this.gameService.save(seededGames);
+	}
+
+	async seedChats() {
+		await this.chatService.trySeed(seedData.chats());
+	}
+
+	async seedUserRelationships() {
+		const allUsers: User[] = await this.userService.findAll({
+			where: [
+				{ name: 'flamink' },
+				{ name: 'vaalboskat' },
+				{ name: 'renoster' },
+			],
+		});
+		const rel: CreateUserRelationshipDto[] = [
+			{
+				source_id: allUsers[0].id,
+				target_id: allUsers[1].id,
+				type: validRelationships.FRIEND,
+			},
+			{
+				source_id: allUsers[0].id,
+				target_id: allUsers[2].id,
+				type: validRelationships.BLOCKED,
+			},
+		];
+		await this.userRelService.save(rel);
+	}
+
 	async seedDatabase() {
 		if (this.shouldSeed()) {
+			await this.seedUsers();
+			await this.seedGames();
+			await this.seedChats();
+			await this.seedUserRelationships();
 			await this.animalService.trySeed(seedData.animals());
 			this.finilizeSeeding();
 		}
