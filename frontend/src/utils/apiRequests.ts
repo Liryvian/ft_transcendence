aa2e376bf8ef5550601844b1c0d5c3e08c53d193
Ryno95 <rmeiboom@student.codam.nl>
2023-02-07 13:03:44 +0100

    addd profileView script for use of store

--- frontend/src/utils/apiRequests.ts
+++ frontend/src/utils/apiRequests.ts
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
+const apiUrl = "http://localhost:8080/api";
+
 export async function postRequest(path: string, data: any) {
-    return await axios.post(`http://localhost:8080/api/${path}`, data);
+    return await axios.post(`${apiUrl}/${path}`, data);
 }
 
 export async function getRequest(path: string) {
-    return await axios.get(`http://localhost:8080/api/${path}`);
+    return await axios.get(`${apiUrl}/${path}`);
 }
 
 export async function patchRequest(path: string , data: any) {
-    return await axios.patch(`http://localhost:8080/api/${path}`, data);
+    return await axios.patch(`${apiUrl}/${path}`, data);
 }
 
 export async function deleteRequest(path: string) {
-    return await axios.delete(`http://localhost:8080/api/${path}`);
+    return await axios.delete(`${apiUrl}/${path}`);
 }
