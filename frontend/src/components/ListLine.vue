ebe819fe3f9ef010b52e9dbc5fc5c134eb77547c
Vincent <Codam>
2023-02-09 13:31:36 +0100

    works with vincent..

--- frontend/src/components/ListLine.vue
+++ frontend/src/components/ListLine.vue
@@ -1,57 +1,58 @@
 <template>
-<div>
-        <div class="c_media">
-	    	<a href="#" class="c_media__asset" :class="{ 'c_asset--online': true, 'c_asset--offline': false, }">
-	    		<div class="c_asset__circle">
-	    			<img src="profile_picture" alt="" />
-	    		</div>
-	    	</a>
-	    </div>
-	    <a href="#">username</a>
-	    <a href="#">Chat</a>
-	    <a href="#">Add Friend</a>
-	    <a href="#">Block</a>
-</div>
+	<div class="c_media">
+		<a
+			href="#"
+			class="c_media__asset"
+			:class="{ 'c_asset--online': true, 'c_asset--offline': false }"
+		>
+			<div class="c_asset__circle">
+				<img src="profile_picture" alt="" />
+			</div>
+		</a>
+	</div>
+	<a href="#">username</a>
+	<a href="#">Chat</a>
+	<a href="#">Add Friend</a>
+	<a href="#">Block</a>
 </template>
 
 <script lang="ts">
 import { defineComponent, type PropType } from 'vue';
-import HorizontalAvatarAndUserName from "@/components/user-info/HorizontalAvatarAndUserName.vue"
+import HorizontalAvatarAndUserName from '@/components/user-info/HorizontalAvatarAndUserName.vue';
 import type { User } from '@/types/User';
 import { useUserStore } from '@/stores/userStore';
 
 export default defineComponent({
-    name: "ListLine",
+	name: 'ListLine',
 
-    setup() {
-        const userStore = useUserStore()
-        userStore.refreshData();
+	setup() {
+		const userStore = useUserStore();
+		userStore.refreshData();
 
-        return {
-            userStore,
-        }
-    },
-  
-    components: {
-		HorizontalAvatarAndUserName
-    },
+		return {
+			userStore,
+		};
+	},
 
-    props: {
-        user: {
-            type: Object as PropType<User>,
-            required: true,
-        },
-        online: Boolean
-    },
-    methods: {
-        createChat(user: User | undefined) {
-            if (user) {
-                console.log(`Starting chat with ${user.name}`);
-            }
-            else {
-                console.log("No User")
-            }
-        }
-    }
-})
+	components: {
+		HorizontalAvatarAndUserName,
+	},
+
+	props: {
+		user: {
+			type: Object as PropType<User>,
+			required: true,
+		},
+		online: Boolean,
+	},
+	methods: {
+		createChat(user: User | undefined) {
+			if (user) {
+				console.log(`Starting chat with ${user.name}`);
+			} else {
+				console.log('No User');
+			}
+		},
+	},
+});
 </script>
--- frontend/src/components/ProfileList.vue
+++ frontend/src/components/ProfileList.vue
@@ -1,111 +1,78 @@
 <template>
-  <div class="c_profileslist__table">
-    <!-- start of "row" component -->
-      <ListLine v-for="user in userStore.allUsers" :user="user"/>
-    <!-- <div class="c_media">
-      <a
-        href="#"
-        class="c_media__asset"
-        :class="{
-          'c_asset--online': true,
-          'c_asset--offline': false,
-        }"
-      >
-        <div class="c_asset__circle">
-          <img src="profile_picture" alt="" />
-        </div>
-      </a>
-    </div>
-    <a href="#">username</a>
-    <a href="#">Chat</a>
-    <a href="#">Add Friend</a>
-    <a href="#">Block</a>
-    end of "row" component -->
-
-    <!-- start of "row" component -->
-    <!-- <div class="c_media">
-      <a
-        href="#"
-        class="c_media__asset"
-        :class="{
-          'c_asset--online': true,
-          'c_asset--offline': false,
-        }"
-      >
-        <div class="c_asset__circle">
-          <img src="profile_picture" alt="" />
-        </div>
-      </a>
-    </div>
-    <a href="#">very long username</a>
-    <a href="#">Chat</a>
-    <a href="#">Add Friend</a>
-    <a href="#">Block</a> -->
-    <!-- end of "row" component -->
-  </div>
+	<div class="c_profileslist__table">
+		<template v-for="user in userStore.allUsers">
+			<ListLine :user="user" />
+		</template>
+	</div>
 </template>
 
 <script lang="ts">
 import { useUserStore } from '@/stores/userStore';
 import { defineComponent } from 'vue';
-import ProfileView_tmp from '@/views/ProfileView-tmp.vue'
+import ProfileView_tmp from '@/views/ProfileView-tmp.vue';
 import ListLine from '@/components/listLine.vue';
 import type { User } from '@/types/User';
 
 export default defineComponent({
-  name: 'ProfileList',
-  components: {
-    ProfileView_tmp,
-    ListLine
-  },
-  setup() {
-    const userStore = useUserStore()
-    const { login, refreshData, isBlocked, isFriend, updateRelationship, allUsers, me } = userStore;
-    refreshData();
+	name: 'ProfileList',
+	components: {
+		ProfileView_tmp,
+		ListLine,
+	},
+	setup() {
+		const userStore = useUserStore();
+		const {
+			login,
+			refreshData,
+			isBlocked,
+			isFriend,
+			updateRelationship,
+			allUsers,
+			me,
+		} = userStore;
+		refreshData();
 
-    return {
-      userStore,
-      login,
-      refreshData,
-      isBlocked,
-      isFriend,
-      updateRelationship,
-      allUsers,
-      me
-    }
-  },
+		return {
+			userStore,
+			login,
+			refreshData,
+			isBlocked,
+			isFriend,
+			updateRelationship,
+			allUsers,
+			me,
+		};
+	},
 
-  methods: {
-    viewProfile(user: User) {
-      console.log(`Profile of ${user.name} clicked`);
-    },
-    createChat(user: User) {
-      console.log(`Starting chat with ${user.name}`);
-    }
-  }
- })
+	methods: {
+		viewProfile(user: User) {
+			console.log(`Profile of ${user.name} clicked`);
+		},
+		createChat(user: User) {
+			console.log(`Starting chat with ${user.name}`);
+		},
+	},
+});
 </script>
 
-
- <style>
- .avatar {
-  vertical-align: middle;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
+<style>
+.avatar {
+	vertical-align: middle;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
 }
 
 .grayedOut {
-  color: grey;
+	color: grey;
 }
 
 button {
-  all: unset;
-  cursor: pointer;
+	all: unset;
+	cursor: pointer;
 }
 
 button:focus {
-  outline: blue 5px auto;
+	outline: blue 5px auto;
 }
 </style>
-
