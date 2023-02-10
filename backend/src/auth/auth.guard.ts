f93a9772eb99a5f104412453031b3c2ae33dadb8
Ryno95 <rmeiboom@student.codam.nl>
2023-02-10 19:12:53 +0100

    Auth gaurd

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -8,7 +8,7 @@ export class AuthGuard implements CanActivate {
 	canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		return true;
+		// return true;
 
 		const request = context.switchToHttp().getRequest();
 		try {
