cb169ffb54725b411c7fdb61973b3716c2cc3a95
Ryno95 <rmeiboom@student.codam.nl>
2023-02-21 12:36:55 +0100

    addded auth guards to /chats and /me

--- backend/src/chats/chat/chat.module.ts
+++ backend/src/chats/chat/chat.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entities/chat.entity';
+import { AuthModule } from '../../auth/auth.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Chat])],
+	imports: [
+		TypeOrmModule.forFeature([Chat]),
+		forwardRef(() => AuthModule),
+		SharedModule,
+	],
 	controllers: [ChatController],
 	providers: [ChatService],
 	exports: [ChatService],
