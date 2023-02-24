5c2bd09d72c102f70c9e4712796112a80e9e166f
Vincent <1089484+vincentvis@users.noreply.github.com>
2023-02-24 16:24:05 +0100

    Vi/chat socket clean (#157)
    * componentize start
    
    * fully untested, but chat list should be okay now?
    
    * going somewhere!
    
    * components in components in components
    
    * it's all components now
    
    * update chat css from old branch
    
    * this runs.. yay.. thanks Ryno
    
    * Successfully connected the dm/channel list to backend! (without socket updates)
    
    * working on message store
    
    * refactor message a bit
    and load messages from api
    
    * update chat css for medium screen size
    
    * a change to message entity that does not break tests.. fun
    
    * not receiving data from socket yet on frontend
    
    * fix seed data
    
    * socket rooms are annoying
    
    * cleanup
    
    * a chat is added.. somewhat..
    
    * can now be subscribed to chatList for updates to chatlist
    
    * cleanup old shit
    
    * going somewhere
    
    * on my way to woring socets
    
    * create chat with users in it
    
    * can now make a chat with users, fun
    
    * joining also seems to work
    
    * seems to all work..
    
    * next todo is in chatstore
    
    * try pre-commit
    
    * pre-commit test
    
    * notes
    
    * remove comments
    
    * fix to import tyeps
    
    * try fixing tests
    
    * wtf was happening here..
    
    * not going to writ tests for sockets apparently haha
    importing the module with socket gateway messes everythign up
    
    * rename to be same as backend
    
    * remove unused impor
    
    * remove heading
    
    * remove some unused things
    
    * userstore getMe method was removed
    
    * remove type update option
    remove type requirement from update/delete and get type from existing item
    swap update/delete to check channel first
    
    * add TODO note
    
    * actually use message.sender as fallback (in old version it never reached that point..)
    
    * remove chat property from singlemessage interface
    
    * remove message findAll and findOne (& update tests)
    
    * remove message e2e test file
    
    * example comments
    
    ---------
    
    Co-authored-by: Vincent <Codam>
    Co-authored-by: Vincent <>
    Co-authored-by: Ryno95 <rmeiboom@student.codam.nl>

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,25 +1,24 @@
 <template>
-	<div >
-
+	<div>
 		<div class="page_box_wrapper">
 			<PlayerNames player_left="renoster" player_right="aardwolf" />
 
-				<div class="page_box">
-					<div class="gameHeader space-between">
-						<p class="playerOneSore"> 5 </p>
-						<p class="pongHeader">PONG</p>
-						<p class="playerTwoScore"> 3 </p>
-					</div>
+			<div class="page_box">
+				<div class="gameHeader space-between">
+					<p class="playerOneSore">5</p>
+					<p class="pongHeader">PONG</p>
+					<p class="playerTwoScore">3</p>
+				</div>
 
-					<canvas
+				<canvas
 					ref="GameRef"
 					id="GameCanvas"
 					class="gameBlock"
-					width="1200"  
-					height="960">
-					</canvas>
-
-				</div>
+					width="1200"
+					height="960"
+				>
+				</canvas>
+			</div>
 		</div>
 	</div>
 </template>
@@ -27,8 +26,8 @@
 <script lang="ts">
 import { useGameStore } from '@/stores/gameStore';
 import { defineComponent } from 'vue';
-import PlayerNames from '@/components/game-info/PlayerNames.vue'
-import type  { ElementPositions, Ball, Paddle }  from "@/types/game.fe"
+import PlayerNames from '@/components/game-info/PlayerNames.vue';
+import type { ElementPositions, Ball, Paddle } from '@/types/game.fe';
 import { io, Socket } from 'socket.io-client';
 
 interface MovementKeys {
@@ -49,18 +48,18 @@ interface DataObject {
 }
 
 export default defineComponent({
-	name: "GameView",
+	name: 'GameView',
 	components: {
-		PlayerNames
+		PlayerNames,
 	},
 
 	beforeRouteLeave(to, from, next) {
 		this.done = true;
-		this.socket.off("updatePosition", this.render);
-		console.log("Game disconnected")
+		this.socket.off('updatePosition', this.render);
+		console.log('Game disconnected');
 		next();
 	},
-	
+
 	data(): DataObject {
 		return {
 			context: {} as CanvasRenderingContext2D,
@@ -75,18 +74,18 @@ export default defineComponent({
 			timeStampStart: 0,
 			previousTimeStamp: 0,
 			done: false,
-		}
+		};
 	},
 
-	setup(){
+	setup() {
 		const gameStore = useGameStore();
 		gameStore.refreshData();
 		return {
 			gameStore,
-		}
+		};
 	},
 
- 	computed: {
+	computed: {
 		// typescript wants values used from html to have a type
 		width() {
 			return (this.$refs.GameRef as HTMLCanvasElement).width;
@@ -98,69 +97,65 @@ export default defineComponent({
 
 	methods: {
 		heightPercentage(percent: number): number {
-			return this.height / 100 * percent;
+			return (this.height / 100) * percent;
 		},
 		widthPercentage(percent: number) {
-			return this.width / 100 * percent;
+			return (this.width / 100) * percent;
 		},
-		
+
 		// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
 		drawMiddleLine() {
 			const heightStart: number = 0;
 			const lineWidth: number = this.widthPercentage(0.3);
-			const widthStart: number = this.widthPercentage(50 - (lineWidth / 2))
+			const widthStart: number = this.widthPercentage(50 - lineWidth / 2);
 			const lineHeight: number = this.heightPercentage(100);
-			
+
 			this.context.fillRect(
-					widthStart,
-					heightStart,
-					lineWidth,
-					lineHeight
-				);
-			},
+				widthStart,
+				heightStart,
+				lineWidth,
+				lineHeight,
+			);
+		},
 
 		//  https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
 		drawBall(ball: Ball) {
 			const startingX: number = this.widthPercentage(ball.position.x);
-			const startingY: number = this.heightPercentage(ball.position.y)
+			const startingY: number = this.heightPercentage(ball.position.y);
 			const radius: number = this.widthPercentage(ball.radius);
 			const startAngle: number = 0;
 			const endAngle: number = Math.PI * 2; // full circle
 
 			this.context.beginPath();
 			this.context.arc(
-					startingX,
-					startingY,
-					radius,
-					startAngle,
-					endAngle,
-			)
-				this.context.lineWidth = this.widthPercentage(0.3);
-				this.context.stroke()
-			},
-				
+				startingX,
+				startingY,
+				radius,
+				startAngle,
+				endAngle,
+			);
+			this.context.lineWidth = this.widthPercentage(0.3);
+			this.context.stroke();
+		},
+
 		// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
 		drawPaddle(paddle: Paddle) {
 			const lineWidth: number = this.widthPercentage(1);
 			const lineHeight: number = this.heightPercentage(12);
-			const y: number = this.heightPercentage(paddle.position.y) - (lineHeight / 2); // middle of the canvas
+			const y: number =
+				this.heightPercentage(paddle.position.y) - lineHeight / 2; // middle of the canvas
 
 			// Player 1
-			let x = 0
+			let x = 0;
 			if (paddle.position.x === 100) {
 				// Player 2
 				x = this.width - lineWidth;
 			}
-			this.context.fillRect(
-				x,
-				y,
-				lineWidth,
-				lineHeight
-			)
+			this.context.fillRect(x, y, lineWidth, lineHeight);
 		},
 
 		clearCanvas() {
-			this.context.clearRect(0, 0, this.width, this.height)
+			this.context.clearRect(0, 0, this.width, this.height);
 		},
 
 		render(elementPositions: ElementPositions) {
@@ -170,18 +165,17 @@ export default defineComponent({
 			this.drawPaddle(elementPositions.playerTwoPaddle);
 			this.drawBall(elementPositions.ball);
 		},
-		
+
 		keyDown(keyPress: KeyboardEvent) {
 			if (this.isPressed[keyPress.key] !== undefined) {
 				this.isPressed[keyPress.key] = true;
 			}
-			
 		},
-		
-    	keyUp(keyPress: KeyboardEvent) {
+
+		keyUp(keyPress: KeyboardEvent) {
 			if (this.isPressed[keyPress.key] !== undefined) {
 				this.isPressed[keyPress.key] = false;
-      		}
+			}
 		},
 
 		// MAIN GAME LOOP
@@ -194,34 +188,32 @@ export default defineComponent({
 			// redraws after intervalMs
 			if (elapsedTime > intervalMs) {
 				this.previousTimeStamp = timeStamp;
-				this.socket!.emit("updatePositions", this.isPressed)
+				this.socket!.emit('updatePositions', this.isPressed);
 			}
 
 			if (!this.done) {
 				window.requestAnimationFrame(this.getUpdatedPositions);
-			}
-			else {
-				console.log("DONE")!
+			} else {
+				console.log('DONE')!;
 			}
 		},
-		
+
 		startGameLoop() {
 			window.requestAnimationFrame(this.getUpdatedPositions);
-		}
+		},
 	},
-	
-	mounted() {
 
+	mounted() {
 		this.context = (this.$refs.GameRef as any).getContext('2d');
-		document.addEventListener("keydown", this.keyDown);
-		document.addEventListener("keyup", this.keyUp);
-		
-		this.socket.on("elementPositions", this.render);
+		document.addEventListener('keydown', this.keyDown);
+		document.addEventListener('keyup', this.keyUp);
+
+		this.socket.on('elementPositions', this.render);
 		// MAIN GAME LOOP
-		this.startGameLoop()
+		this.startGameLoop();
 	},
-})
-</script> 
+});
+</script>
 
 <style>
 .gameBlock {
@@ -229,14 +221,14 @@ export default defineComponent({
 	width: 90%;
 	display: block;
 	margin: auto;
-  	transform: translateY(5%);
+	transform: translateY(5%);
 }
 
-.gameHeader{
+.gameHeader {
 	font-weight: bolder;
 	display: flex;
 	/* font size changes on window HEIGHT! */
-	font-size: 2.5vh; 
+	font-size: 2.5vh;
 	padding-left: 2.5%;
 	padding-right: 5%;
 	padding-top: 2.4%;
@@ -245,5 +237,4 @@ export default defineComponent({
 .gameHeader.space-between {
 	justify-content: space-between;
 }
-
 </style>
