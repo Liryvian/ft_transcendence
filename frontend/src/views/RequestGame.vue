bc74478a031f218a71469c7452f29fd22aa834cc
Lisa Vlamings <livlamin@f1r6s15.codam.nl>
2023-02-24 16:39:39 +0100

    start

--- frontend/src/views/RequestGame.vue
+++ frontend/src/views/RequestGame.vue
@@ -6,9 +6,20 @@
 			<h1>PLAYER_02</h1>
 			<div class="c_block c_form_group">
 				<div class="c_field_group">
-					<InputField label="score_to_win" v-model="score_to_win"/>
-					<InputField label="background_colo" v-model="background_color"/>
-					<corner-button link_text="request" link_target="/" position="pb_bottom"/>
+					<InputField
+						label="score_to_win"
+						:modelValue="score_to_win"
+						@update:modelValue="updateProfileForm.name = $event"
+					/>
+					<InputField
+						label="background_colo"
+						v-model="background_color"
+					/>
+					<corner-button
+						link_text="request"
+						link_target="/"
+						position="pb_bottom"
+					/>
 				</div>
 			</div>
 		</div>
@@ -19,14 +30,29 @@
 import { defineComponent } from 'vue';
 import InputField from '@/components/input-fields/InputField.vue';
 import CornerButton from '@/components/buttons/CornerButton.vue';
+import { useGameStore } from '@/stores/gameStore';
 
 export default defineComponent({
 	name: 'GameRequest',
 	components: {
 		InputField,
-		CornerButton
+		CornerButton,
 	},
 
+	async created() {
+		await useGameStore().refreshMyGames();
+	},
+
+	setup() {
+		const gameStore = useGameStore();
+		useGameStore().refreshMyGames();
+		let createGameForm: CreateGameForm = reactive({
+
+		});
+		return {
+			gameStore,
+		};
+	}
 
 });
 </script>
