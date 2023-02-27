69f5e9f3ceaccc70281e1b705e7cff8976286d21
Lisa Vlamings <livlamin@f0r4s24.codam.nl>
2023-02-27 14:43:30 +0100

    only realtionship check and than its done

--- backend/src/pong/game/game.service.ts
+++ backend/src/pong/game/game.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AbstractService } from '../../shared/abstract.service';
 import { Repository } from 'typeorm';
 import { Game } from './entities/game.entity';
+import { UserRelationship } from '../../users/user-relationship/entities/user-relationship.entity';
+import { CreateUserRelationshipDto } from '../../users/user-relationship/dto/create-user-relationship.dto';
 
 @Injectable()
 export class GameService extends AbstractService<Game> {
@@ -13,3 +15,39 @@ export class GameService extends AbstractService<Game> {
 		super(repository);
 	}
 }
+// 	//  check that id: a - b and id: b - a doesn't already exists
+// 	async getExistingRelationship(
+// 		player_one: number,
+// 		player_twp: number,
+// 	): Promise<UserRelationship> | null {
+// 		try {
+// 			const relationship: UserRelationship = await this.findOne({
+// 				relations: {
+// 					player_one_id: true,
+// 					player_twp_id: true,
+// 				},
+// 				where: {
+// 					player_one_id: {
+// 						id: In([player_one, player_twp]),
+// 					},
+// 					player_twp_id: {
+// 						id: In([player_one, player_twp]),
+// 					},
+// 				},
+// 			});
+// 			return relationship;
+// 		} catch (e) {
+// 			return null;
+// 		}
+// 	}
+//
+// 	async hasExistingRelationship(
+// 		relationsRequest: CreateUserRelationshipDto,
+// 	): Promise<boolean> {
+// 		const relation: UserRelationship = await this.getExistingRelationship(
+// 			relationsRequest.player_one_id,
+// 			relationsRequest.player_twp_id,
+// 		);
+// 		return relation !== null;
+// 	}
+// }
