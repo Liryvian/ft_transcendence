2a2e2470c07208ee6599dcd7ed17a6ff8e57a6dd
Vincent <Codam>
2023-03-02 13:21:53 +0100

    cleanup service file

--- backend/src/auth/twofa/twofa.service.ts
+++ backend/src/auth/twofa/twofa.service.ts
@@ -1,25 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { UserService } from '../../users/user/user.service';
-import * as crypto from 'crypto';
 import * as QRCode from 'qrcode';
 import { authenticator } from 'otplib';
 import { TwoFaDto } from './dto/twofa.dto';
 
-// we might want to consider encrypting/decrypting the secret
-// instead of saving it plaintext into the db
-//
-// import { scrypt, randomFill, createCipheriv } from 'crypto';
-// https://nodejs.org/api/crypto.html#class-cipher
-//
-
 @Injectable()
 export class TwoFaService {
-	constructor(
-		private userService: UserService,
-		private configService: ConfigService,
-	) {}
-
 	async generateSecret() {
 		const secret = authenticator.generateSecret();
 		const otpauthUrl = authenticator.keyuri('userName', 'liryvianPong', secret);
@@ -29,8 +14,6 @@ export class TwoFaService {
 				return url;
 			})
 			.catch((err) => {
-				// not sure how to handle this or how this errors..
-				console.error(err);
 				return '';
 			});
 
