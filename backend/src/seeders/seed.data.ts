af3c8d4c28fb1aa16d2ff8c01fbae716523d72a0
Vincent <Codam>
2023-03-02 11:41:19 +0100

    seeding failed, sorry

--- backend/src/seeders/seed.data.ts
+++ backend/src/seeders/seed.data.ts
@@ -225,12 +225,14 @@ const seedData = {
 		const zoo = chats.find((c) => c.name === 'Zoo').id;
 		const zzzSleepy = chats.find((c) => c.name === 'Zzz sleepy').id;
 
-		// Add flamink and renoster to their DM
-		[(permissionsEnum.POST, permissionsEnum.READ)].forEach((p) => {
+		[
+			// Add flamink and renoster to their DM
+			(permissionsEnum.POST, permissionsEnum.READ),
+		].forEach((p) => {
 			[flamink, renoster].forEach((u) => {
 				cups.push({
-					chat_id: flaminkRenoster.id,
-					user_id: u.id,
+					chat_id: flaminkRenoster,
+					user_id: u,
 					permission: p,
 				});
 			});
@@ -240,8 +242,8 @@ const seedData = {
 		[permissionsEnum.POST, permissionsEnum.READ].forEach((p) => {
 			[vaalboskat, renoster].forEach((u) => {
 				cups.push({
-					chat_id: vaalboskatRenoster.id,
-					user_id: u.id,
+					chat_id: vaalboskatRenoster,
+					user_id: u,
 					permission: p,
 				});
 			});
@@ -254,8 +256,8 @@ const seedData = {
 			permissionsEnum.POST,
 		].forEach((p) => {
 			cups.push({
-				chat_id: desert.id,
-				user_id: renoster.id,
+				chat_id: desert,
+				user_id: renoster,
 				permission: p,
 			});
 		});
@@ -269,8 +271,8 @@ const seedData = {
 		].forEach((p) => {
 			[aardwolf, flamink, renoster, vaalboskat].forEach((u) => {
 				cups.push({
-					chat_id: zoo.id,
-					user_id: u.id,
+					chat_id: zoo,
+					user_id: u,
 					permission: p,
 				});
 			});
@@ -281,8 +283,8 @@ const seedData = {
 			.map(
 				(u) =>
 					({
-						chat_id: desert.id,
-						user_id: u.id,
+						chat_id: desert,
+						user_id: u,
 						permission: permissionsEnum.READ,
 					} as CreateChatUserPermissionDto),
 			)
@@ -291,28 +293,28 @@ const seedData = {
 		// add wildsbok user blocked + left to every chat
 		[desert, zoo]
 			.map((c) => ({
-				chat_id: c.id,
-				user_id: wildsbok.id,
+				chat_id: c,
+				user_id: wildsbok,
 				permission: permissionsEnum.BLOCKED,
 			}))
 			.forEach((cup) => cups.push(cup));
 		[desert, zoo]
 			.map((c) => ({
-				chat_id: c.id,
-				user_id: wildsbok.id,
+				chat_id: c,
+				user_id: wildsbok,
 				permission: permissionsEnum.LEFT,
 			}))
 			.forEach((cup) => cups.push(cup));
 
 		// add wildsbok to Zzz sleepy channel
 		cups.push({
-			chat_id: zzzSleepy.id,
-			user_id: wildsbok.id,
+			chat_id: zzzSleepy,
+			user_id: wildsbok,
 			permission: permissionsEnum.POST,
 		});
 		cups.push({
-			chat_id: zzzSleepy.id,
-			user_id: wildsbok.id,
+			chat_id: zzzSleepy,
+			user_id: wildsbok,
 			permission: permissionsEnum.READ,
 		});
 		return cups;
