715d454005bb2f15380726043af6db81b0101941
Vincent <>
2023-03-03 20:33:47 +0100

    css update for game view stuff

--- frontend/src/components/pongGame/ActiveGameRow.vue
+++ frontend/src/components/pongGame/ActiveGameRow.vue
@@ -1,41 +1,27 @@
 <template>
-	<div class="">{{ player_left }} {{ player_left_score }}</div>
-    <!-- MIDDLE LINE HERE -->
-	<div class="">{{ player_right_score }} {{ player_right }}</div>
-
-    <router-link :to="{name: 'pong'}"> [watch game]</router-link>
+	<div class="table_row">
+		<div class="table_cell table_left">
+			{{ player_left }} {{ player_left_score }}
+		</div>
+		<div class="table_cell table_right table_extra">
+			{{ player_right_score }} {{ player_right }}
+			<router-link :to="{ name: 'pong' }"> [watch game] </router-link>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import type { Game } from "@/types/game.fe";
-import {defineComponent, type PropType} from "vue";
+import type { Game } from '@/types/game.fe';
+import { defineComponent, type PropType } from 'vue';
 
 export default defineComponent({
 	name: 'ActiveGameRow',
 	props: {
 		player_left: String,
-        player_left_score: Number,
+		player_left_score: Number,
 		player_right: String,
-        player_right_score: Number,
-        game_id: Number
+		player_right_score: Number,
+		game_id: Number,
 	},
 });
 </script>
-
-<style scoped>
-
-.lefty {
-	position: absolute;
-	left: 25%;
-	top: 50%;
-	/* writing-mode: vertical-lr; */
-	/* text-orientation: mixed; */
-	/* color: rgb(120, 117, 117); */
-}
-
-.righty {
-	text-align: right;
-	left: auto;
-	right: 25%;
-}
-</style>
