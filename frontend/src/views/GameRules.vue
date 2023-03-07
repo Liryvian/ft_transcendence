b4cae8ff5978651f220dc1aa90b92375b61f9ab6
Lisa Vlamings <livlamin@f1r6s15.codam.nl>
2023-03-07 11:32:59 +0100

    rules done?

---
+++ frontend/src/views/GameRules.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<div class="page_box_wrapper">
+			<div class="page_box">
+				<h1>GAME RULES</h1>
+				<div class="table c_block">
+					- a default game is played to 10 points<br />
+					- movement: 'W' = up & 'S' = down<br />
+					- Game starts straight after accepting a game request<br />
+				</div>
+
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+</script>
+
